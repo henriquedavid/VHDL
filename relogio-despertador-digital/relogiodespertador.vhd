3467f18680a36ddfd3acaacccda521eca368f05c
Henrique David de Medeiros <henriquemed101@gmail.com>
2019-09-21 16:07:36 -0300

    Conclusão do projeto.

--- relogio-despertador-digital/relogiodespertador.vhd
+++ relogio-despertador-digital/relogiodespertador.vhd
@@ -1,5 +1,5 @@
 ENTITY relogiodespertador IS
-	PORT ( a				  	 :  IN BIT_VECTOR( 8 DOWNTO 0);
+	PORT ( a				  	 :  IN BIT_VECTOR( 9 DOWNTO 0);
 			 switch						 :  IN BIT_VECTOR( 1 DOWNTO 0);
 			 o1,o2,o3,o4 							 : OUT BIT_VECTOR(  6 DOWNTO 0 );
 			 a1							 :  IN BIT_VECTOR( 9 DOWNTO 0));
@@ -13,7 +13,7 @@ ARCHITECTURE configuracao OF relogiodespertador IS
 	END COMPONENT;
 	
 	COMPONENT codificadordeteclado IS
-	PORT ( a : IN BIT_VECTOR(8 DOWNTO 0);
+	PORT ( a : IN BIT_VECTOR(9 DOWNTO 0);
 			 o1      		 : OUT BIT_VECTOR( 3 DOWNTO 0 ));
    END COMPONENT;
 	
@@ -28,9 +28,9 @@ ARCHITECTURE configuracao OF relogiodespertador IS
 BEGIN
 	p1 : codificadordeteclado PORT MAP (a, w0); -- entra o numero que o usuarios clicou e sai o valor em 4 bits
 	p2 : mux_4x1 PORT MAP (w0, switch, w1, w2, w3, w4);
-	p3 : decodificador PORT MAP(w1(0), w1(1), w1(2), w1(3), o1);
-	p4 : decodificador PORT MAP(w2(0), w2(1), w2(2), w2(3), o2);
-	p5 : decodificador PORT MAP(w3(0), w3(1), w3(2), w3(3), o3);
-	p6 : decodificador PORT MAP(w4(0), w4(1), w4(2), w4(3), o4);
+	p3 : decodificador PORT MAP (w1(3), w1(2), w1(1), w1(0), o1);
+	p4 : decodificador PORT MAP (w2(3), w2(2), w2(1), w2(0), o2);
+	p5 : decodificador PORT MAP (w3(3), w3(2), w3(1), w3(0), o3);
+	p6 : decodificador PORT MAP (w4(3), w4(2), w4(1), w4(0), o4);
 	
 END configuracao;
